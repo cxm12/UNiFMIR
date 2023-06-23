7ab06dcbcf256bb2d20c1e49ade2504cf71d583e
cxm12 <64177767+cxm12@users.noreply.github.com>
2023-06-23 17:25:36 +0800

    Add files via upload

---
+++ model/__init__.py
@@ -0,0 +1,270 @@
+import os
+import torch
+import torch.nn as nn
+from model.attention import ProjectionUpdater
+from model.Unimodel import UniModel
+
+
+class Model(nn.Module):
+    def __init__(self, args, ckp, srmodel=None):
+        super(Model, self).__init__()
+        print('Making model...')
+
+        self.scale = args.scale[0]
+        self.idx_scale = 0
+        self.self_ensemble = args.self_ensemble
+        self.chop = args.chop
+        self.precision = args.precision
+        self.cpu = args.cpu
+        self.device = torch.device('cpu' if args.cpu else 'cuda')
+        self.n_GPUs = args.n_GPUs
+        self.save_models = args.save_models
+        self.args = args
+        
+        self.model = srmodel.to(self.device)
+        
+        self.proj_updater = ProjectionUpdater(self.model, feature_redraw_interval=640)
+        if args.precision == 'half':
+            self.model.half()
+
+        if not args.cpu and args.n_GPUs > 1:
+            self.model = nn.DataParallel(self.model, range(args.n_GPUs))
+
+        self.load(os.path.join('experiment', args.save, 'model'),  # ckp.dir,
+                  pre_train=args.pre_train, resume=args.resume, cpu=args.cpu)
+        print(self.model, file=ckp.log_file)
+
+    def forward(self, x, idx_scale):
+        self.proj_updater.redraw_projections()
+        self.idx_scale = idx_scale
+        if self.chop and not self.training and self.args.test_only:
+            if 'proj' in self.args.model or self.args.task == 4:
+                return self.forward_chopProj(x)
+            elif '2t3' in self.args.model or self.args.task == 5:
+                return self.forward_chop2to3(x)
+            else:
+                return self.forward_chop(x)
+        else:
+            return self.model(x)
+
+    def get_model(self):
+        return self.model
+
+    def state_dict(self, **kwargs):
+        target = self.get_model()
+        return target.state_dict(**kwargs)
+
+    def save(self, apath, epoch, is_best=False):
+        target = self.get_model()
+        torch.save(
+            target.state_dict(), 
+            os.path.join(apath, 'model_latest.pt')
+        )
+        if is_best:
+            torch.save(
+                target.state_dict(),
+                os.path.join(apath, 'model_best.pt')
+            )
+        
+        if self.save_models:
+            torch.save(
+                target.state_dict(),
+                os.path.join(apath, 'model_{}.pt'.format(epoch))
+            )
+
+    def load(self, apath, pre_train='.', resume=-1, cpu=False):
+        if cpu:
+            kwargs = {'map_location': lambda storage, loc: storage}
+        else:
+            kwargs = {}
+
+        if resume == -1:
+            print('Load Model from ', os.path.join(apath, 'model_latest.pt'))
+            self.model.load_state_dict(
+                torch.load(
+                    os.path.join(apath, 'model_latest.pt'),
+                    **kwargs
+                ),
+                strict=True
+            )
+        elif resume == -2:
+            m = os.path.join(apath, 'model_best.pt')
+            print('Load Model from ', m)
+            self.model.load_state_dict(torch.load(m, **kwargs), strict=True)
+        elif resume < -2:
+            m = os.path.join(apath, 'model_best%d.pt' % -resume)
+            print('Load Model from ', m)
+            self.model.load_state_dict(torch.load(m, **kwargs), strict=True)
+        elif resume == 0 and pre_train != '.':
+            print('Loading UNet model from {}'.format(pre_train))
+            self.get_model().load_state_dict(torch.load(pre_train, **kwargs), strict=False)
+        elif (resume > 0) and os.path.exists(os.path.join(apath, 'model_{}.pt'.format(resume))):
+            print('Load Model from ', os.path.join(apath, 'model_{}.pt'.format(resume)))
+            self.get_model().load_state_dict(
+                torch.load(
+                    os.path.join(apath, 'model_{}.pt'.format(resume)),
+                    **kwargs
+                ),
+                strict=True
+            )
+        else:
+            print('!!!!!!!!  Not Load Model  !!!!!!')
+        
+    def load_network(self, load_path, strict=True, param_key=None):  # 'params'params_ema
+        load_net = torch.load(load_path, map_location=lambda storage, loc: storage)
+        self.model.load_state_dict(load_net, strict=strict)
+        print(f'Loading {self.model.__class__.__name__} model from {load_path}.')
+
+    def _print_different_keys_loading(self, crt_net, load_net, strict=True):
+        crt_net = crt_net.state_dict()
+        crt_net_keys = set(crt_net.keys())
+        load_net_keys = set(load_net.keys())
+
+        # logger = get_root_logger()
+        if crt_net_keys != load_net_keys:
+            print('Current net - loaded net:')
+            for v in sorted(list(crt_net_keys - load_net_keys)):
+                print('warning', f'  {v}')
+            print('warning', 'Loaded net - current net:')
+            for v in sorted(list(load_net_keys - crt_net_keys)):
+                print('warning', f'  {v}')
+
+        # check the size for the same keys
+        if not strict:
+            common_keys = crt_net_keys & load_net_keys
+            for k in common_keys:
+                if crt_net[k].size() != load_net[k].size():
+                    print('warning', f'Size different, ignore [{k}]: crt_net: '
+                                   f'{crt_net[k].shape}; load_net: {load_net[k].shape}')
+                    load_net[k + '.ignore'] = load_net.pop(k)
+
+    def forward_chop(self, x, shave=10, min_size=120000):
+        scale = self.scale  # self.scale[self.idx_scale]
+        n_GPUs = min(self.n_GPUs, 4)
+        b, _, h, w = x.size()
+        h_half, w_half = h // 2, w // 2
+        
+        h_size, w_size = h_half + 16, w_half + 16
+        h_size += 8
+        w_size += 8
+
+        lr_list = [
+            x[:, :, 0:h_size, 0:w_size],
+            x[:, :, 0:h_size, (w - w_size):w],
+            x[:, :, (h - h_size):h, 0:w_size],
+            x[:, :, (h - h_size):h, (w - w_size):w]]
+
+        if w_size * h_size < min_size:
+            sr_list = []
+            for i in range(0, 4, n_GPUs):
+                lr_batch = torch.cat(lr_list[i:(i + n_GPUs)], dim=0)
+                sr_batch = self.model(lr_batch)
+                sr_list.extend(sr_batch.chunk(n_GPUs, dim=0))
+        else:
+            sr_list = [
+                self.forward_chop(patch, shave=shave, min_size=min_size)\
+                for patch in lr_list]
+
+        h, w = scale * h, scale * w
+        h_half, w_half = scale * h_half, scale * w_half
+        h_size, w_size = scale * h_size, scale * w_size
+        shave *= scale
+        
+        output = x.new(b, 1, h, w)
+        output[:, :, 0:h_half, 0:w_half] \
+            = sr_list[0][:, :, 0:h_half, 0:w_half]
+        output[:, :, 0:h_half, w_half:w] \
+            = sr_list[1][:, :, 0:h_half, (w_size - w + w_half):w_size]
+        output[:, :, h_half:h, 0:w_half] \
+            = sr_list[2][:, :, (h_size - h + h_half):h_size, 0:w_half]
+        output[:, :, h_half:h, w_half:w] \
+            = sr_list[3][:, :, (h_size - h + h_half):h_size, (w_size - w + w_half):w_size]
+
+        return output
+
+    def forward_chopProj(self, x, shave=10, min_size=120000):
+        scale = self.scale
+        n_GPUs = min(self.n_GPUs, 4)
+        b, c, h, w = x.size()
+        h_half, w_half = h // 2, w // 2
+        h_size, w_size = h_half + shave, w_half + shave
+        h_size += 4-h_size % 4  # [1~4]
+        w_size += 8-w_size % 8
+        
+        lr_list = [
+            x[:, :, 0:h_size, 0:w_size],
+            x[:, :, 0:h_size, (w - w_size):w],
+            x[:, :, (h - h_size):h, 0:w_size],
+            x[:, :, (h - h_size):h, (w - w_size):w]]
+    
+        if w_size * h_size < min_size:
+            sr_list = []
+            for i in range(0, 4, n_GPUs):
+                lr_batch = torch.cat(lr_list[i:(i + n_GPUs)], dim=0)
+                print('Proj Output')
+                sr_batchgvt, sr_batch = self.model(lr_batch)                
+                sr_list.extend(sr_batch.chunk(n_GPUs, dim=0))
+        else:
+            sr_list = [
+                self.forward_chopProj(patch, shave=shave, min_size=min_size) for patch in lr_list]
+    
+        h, w = scale * h, scale * w
+        h_half, w_half = scale * h_half, scale * w_half
+        h_size, w_size = scale * h_size, scale * w_size
+        shave *= scale
+    
+        output = x.new(b, 1, h, w)
+        output[:, :, 0:h_half, 0:w_half] \
+            = sr_list[0][:, :, 0:h_half, 0:w_half]
+        output[:, :, 0:h_half, w_half:w] \
+            = sr_list[1][:, :, 0:h_half, (w_size - w + w_half):w_size]
+        output[:, :, h_half:h, 0:w_half] \
+            = sr_list[2][:, :, (h_size - h + h_half):h_size, 0:w_half]
+        output[:, :, h_half:h, w_half:w] \
+            = sr_list[3][:, :, (h_size - h + h_half):h_size, (w_size - w + w_half):w_size]
+    
+        return output
+
+    def forward_chop2to3(self, x, shave=2, min_size=120000):
+        scale = 11
+        n_GPUs = min(self.n_GPUs, 4)
+        b, c, h, w = x.size()  # [1, 121, 16, 16]
+        h_half, w_half = h // 2, w // 2
+        
+        h_size, w_size = h_half + shave, w_half + shave
+      
+        h_size += 8-h_size % 8  # h_size += 4-h_size % 4  # [20, 24]
+        w_size += 8-w_size % 8
+        
+        print('0 x.size = ', x.size(), 'h/w_size = ', h_size, w_size)
+        
+        lr_list = [
+            x[:, :, 0:h_size, 0:w_size],
+            x[:, :, 0:h_size, (w - w_size):w],
+            x[:, :, (h - h_size):h, 0:w_size],
+            x[:, :, (h - h_size):h, (w - w_size):w]]
+    
+        if w_size * h_size < min_size:
+            sr_list = []
+            for i in range(0, 4, n_GPUs):
+                lr_batch = torch.cat(lr_list[i:(i + n_GPUs)], dim=0)
+                sr_batchu, sr_batch = self.model(lr_batch)
+                sr_list.extend(sr_batch.chunk(n_GPUs, dim=0))
+        else:
+            sr_list = [self.forward_chop2to3(patch, shave=shave, min_size=min_size) for patch in lr_list]
+    
+        h, w = scale * h, scale * w
+        h_half, w_half = scale * h_half, scale * w_half  # 88, 88
+        h_size, w_size = scale * h_size, scale * w_size  # 352 352
+       
+        output = x.new(b, 61, h, w)
+        output[:, :, 0:h_half, 0:w_half] \
+            = sr_list[0][:, :, 0:h_half, 0:w_half]
+        output[:, :, 0:h_half, w_half:w] \
+            = sr_list[1][:, :, 0:h_half, (w_size - w + w_half):w_size]
+        output[:, :, h_half:h, 0:w_half] \
+            = sr_list[2][:, :, (h_size - h + h_half):h_size, 0:w_half]
+        output[:, :, h_half:h, w_half:w] \
+            = sr_list[3][:, :, (h_size - h + h_half):h_size, (w_size - w + w_half):w_size]
+    
+        return output
